a8630002cbb19b723748899f7a133c1fe56d271d
dominic <dominic@d6cdb5e5-674c-0410-9c8e-b8b9dce9c8c9>
2008-05-04 14:24:06 +0000

    Removal of build files

--- lib/luacc/Debug/error.d
+++
@@ -1,7 +0,0 @@
-/Users/dom/Projects/luacc/lib/luacc/Debug/error.lo \
-  /Users/dom/Projects/luacc/lib/luacc/Debug/error.o \
-  /Users/dom/Projects/luacc/lib/luacc/Debug/error.d \
-  /Users/dom/Projects/luacc/lib/luacc/Debug/error.o: error.cpp \
-  core/error.h
-
-core/error.h:
