24172bab70a506fd4845b0854e63c3f5c3087c3c
dominic <510002+dmah42@users.noreply.github.com>
2008-05-24 15:35:54 +0000

    Clean up some redundant folders

--- tools/luacc/Release/main.d
+++
@@ -1,7 +0,0 @@
-/Users/dom/Projects/luacc/tools/luacc/Release/main.lo \
-  /Users/dom/Projects/luacc/tools/luacc/Release/main.o \
-  /Users/dom/Projects/luacc/tools/luacc/Release/main.d \
-  /Users/dom/Projects/luacc/tools/luacc/Release/main.o: main.cpp \
-  /Users/dom/Projects/llvm/luacc/include/luacc/luacc.h
-
-/Users/dom/Projects/llvm/luacc/include/luacc/luacc.h:
