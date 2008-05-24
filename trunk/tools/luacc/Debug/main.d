a136b98db788a238c8e6a25747028eec3ccaf121
dominic <dominic@d6cdb5e5-674c-0410-9c8e-b8b9dce9c8c9>
2008-05-24 15:35:54 +0000

    Clean up some redundant folders

--- trunk/tools/luacc/Debug/main.d
+++
@@ -1,7 +0,0 @@
-/Users/dom/Projects/luacc/tools/luacc/Debug/main.lo \
-  /Users/dom/Projects/luacc/tools/luacc/Debug/main.o \
-  /Users/dom/Projects/luacc/tools/luacc/Debug/main.d \
-  /Users/dom/Projects/luacc/tools/luacc/Debug/main.o: main.cpp \
-  /Users/dom/Projects/llvm/luacc/include/luacc/luacc.h
-
-/Users/dom/Projects/llvm/luacc/include/luacc/luacc.h:
